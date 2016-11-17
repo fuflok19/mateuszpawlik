14d63edcf42dbbaa8cc6379b1ce03aa004c3599a
Mateusz <mateusz.adam.pawlik@gmail.com>
2016-11-17 11:07:51 +0100

    nav

--- js/app.js
+++ js/app.js
@@ -6,20 +6,62 @@ $(function(){
          .next('img').fadeIn(1000)
          .end().appendTo('.slider');}, 
       3000);
-});
 
-$(function () {
 
 
     var spanToLoad = $('.loading');
 
     spanToLoad.each(function () {
-        $(this).animate({width: $(this).data('procent') + "%"}, $(this).data('procent') / 100 * 5000);
+        var bar = $(this);
+        var id = setInterval(function(){
+            var text = parseInt(bar.width()/3);
+                bar.text(text+'%');
+                console.log('wywoływanie interval dla'+ id);
+        }
+        ,100
+        );
+        $(this).animate({width: $(this).data('procent') + "%"}, $(this).data('procent') / 100 * 5000, function(){
+            console.log(id);
+            clearInterval(id);
+        });
     });
 
+    var menu = $('.menu');
+    var Links = menu.find('a');
+
+    Links.on('click', function(e){
+        e.preventDefault();
+        var speed = 2;
+        var href = $(this).attr('href');
+        console.log(href);
+
+        var position = $(href).offset();            
+        console.log(position.top);
+        var time = position.top/speed;
+        console.log(time);
+        $('html, body').animate({scrollTop: position.top}, time);
+    });
+
+    var back = $('.footer');
+        console.log(back);
+    var goTop = back.find('a');
+        console.log(goTop);
+
+    goTop.on('click', function(e){
+        e.preventDefault();
+        var gogo = 2;
+        var href = $(this).attr('href');
+        console.log(href);
+
+        var position = $(href).offset();            
+        console.log(position.top);
+        var time = position.top/gogo;
+        console.log(time);
+        $('html, body').animate({scrollTop: position.top}, time);
+    });
+
+
 });
-
-
 
 
 
