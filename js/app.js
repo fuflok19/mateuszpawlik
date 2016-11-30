1f6a1a61802a946b402ab27ff445fdcf1722b094
Mateusz <mateusz.adam.pawlik@gmail.com>
2016-11-30 16:14:58 +0100

    nav

--- js/app.js
+++ js/app.js
@@ -1,38 +1,10 @@
 
-$(function(){
-    $('.slider img:gt(0)').hide();
-    setInterval(function(){
-      $('.slider :first-child').fadeOut(1000)
-         .next('img').fadeIn(1000)
-         .end().appendTo('.slider');}, 
-      3000);
-});
-
-
-    var spanToLoad = $('.loading');
-  
-    spanToLoad.each(function () {
-        var bar = $(this);
-        var id = setInterval(function(){
-            var text = parseInt(bar.width()/3);
-                bar.text(text+'%');
-                console.log('wywoływanie interval dla'+ id);
-        }
-        ,100
-        );
-        $(this).animate({width: $(this).data('procent') + "%"}, $(this).data('procent') / 100 * 5000,function(){
-            console.log(id);
-            clearInterval(id);
-        });
-      });
-
-
     var menu = $('.menu');
     var Links = menu.find('a');
 
     Links.on('click', function(e){
         e.preventDefault();
-        var speed = 2;
+        var speed = 1;
         var href = $(this).attr('href');
         console.log(href);
 
