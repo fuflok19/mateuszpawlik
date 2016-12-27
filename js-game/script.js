8d476cc56caba5caf026f3767165c9a726e05d96
Mateusz <mateusz.adam.pawlik@gmail.com>
2016-12-27 18:04:20 +0100

    bricks

--- js-game/script.js
+++ js-game/script.js
@@ -10,9 +10,26 @@ var dy = -2;
 var ballRadius = 10;
 var paddleHeight = 10;
 var paddleWidth = 75;
-var padleX = (canvas.width-paddleWidth)/2;
+var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (c=0; c<brickColumnCount; c++){
+	bricks[c] = [];
+	for (r=0; r<brickRowCount; r++){
+		bricks[c][r] = {x: 0, y:0}
+	}
+}
+
+
 
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
@@ -56,6 +73,17 @@ function draw(){
 	drawBall();
 	drawPaddle();
 
+	if(y + dy < ballRadius) {
+		dy = -dy;
+	} else if (y + dy > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+				dy = -dy;
+		} else {
+		alert("GAME OVER - KONIEC GRY!");
+		document.location.reload();
+		}
+	}
+
 	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
 		dy = -dy;
 	}
@@ -73,3 +101,5 @@ function draw(){
 	x += dx;
 	y += dy;
 }
+
+setInterval(draw, 1000);
